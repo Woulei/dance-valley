8a1d5d82afa7df0a31299bec15e515fa28f598e6
Woulei <woulei@me.com>
2017-03-10 12:59:19 +0100

    rename list to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  
-  get "stages" => "stages#list"
-  get "stages/:id" => "stages#show", as: :stage
+
+  resources :artists
+  resources :stages
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#index"
-
   get "dance_valley" => "pages#index"
 
 end
