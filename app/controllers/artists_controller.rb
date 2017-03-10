b79fcc12d7bd3bda297909d8774d99ca6ab173cb
FLORISSP <fjspape@gmail.com>
2017-03-10 14:15:20 +0100

    Created a show and new page for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,20 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist_params = params.require(:artist).permit(:name, :description, :image_url, :website)
+
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      rederict_to @artist
+    else
+      render 'new'
+    end
+  end
 end
